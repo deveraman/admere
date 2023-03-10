3d2d2be7f3aa153ccda2f199c707c460cac3edce
ramanverma2k <ramanverma4183@gmail.com>
2023-03-10 12:48:17 +0530

    feat: add timeline module for SET B task

---
+++ lib/src/timeline/timeline_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TimelinePage extends StatefulWidget {
+  const TimelinePage({super.key});
+
+  @override
+  _TimelinePageState createState() => _TimelinePageState();
+}
+
+class _TimelinePageState extends State<TimelinePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
